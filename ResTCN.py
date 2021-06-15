be71c0c64320429a9d630f0f754c945d7a26ce85
abedicodes <73629756+abedicodes@users.noreply.github.com>
2021-06-14 21:33:47 -0400

    Update ResTCN.py

--- ResTCN.py
+++ ResTCN.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18
-from torch.nn.utils import weight_norm
+import torchvision
+
+from TCN import TemporalConvNet
 
 
 class Identity(nn.Module):
@@ -12,99 +13,53 @@ class Identity(nn.Module):
         return x
 
 
-class Chomp1d(nn.Module):
-    def __init__(self, chomp_size):
-        super(Chomp1d, self).__init__()
-        self.chomp_size = chomp_size
-
-    def forward(self, x):
-        return x[:, :, :-self.chomp_size].contiguous()
-
-
-class TemporalBlock(nn.Module):
-    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
-        super(TemporalBlock, self).__init__()
-        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
-                                           stride=stride, padding=padding, dilation=dilation))
-        self.chomp1 = Chomp1d(padding)
-        self.relu1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(dropout)
-
-        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
-                                           stride=stride, padding=padding, dilation=dilation))
-        self.chomp2 = Chomp1d(padding)
-        self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(dropout)
-
-        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
-                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
-        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
-        self.relu = nn.ReLU()
-        self.init_weights()
-
-    def init_weights(self):
-        self.conv1.weight.data.normal_(0, 0.01)
-        self.conv2.weight.data.normal_(0, 0.01)
-        if self.downsample is not None:
-            self.downsample.weight.data.normal_(0, 0.01)
-
-    def forward(self, x):
-        out = self.net(x)
-        res = x if self.downsample is None else self.downsample(x)
-        return self.relu(out + res)
-
-
-class TemporalConvNet(nn.Module):
-    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
-        super(TemporalConvNet, self).__init__()
-        layers = []
-        num_levels = len(num_channels)
-        for i in range(num_levels):
-            dilation_size = 2 ** i
-            in_channels = num_inputs if i == 0 else num_channels[i - 1]
-            out_channels = num_channels[i]
-            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
-                                     padding=(kernel_size - 1) * dilation_size, dropout=dropout)]
-
-        self.network = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.network(x)
-
-
 class ResTCN(nn.Module):
     def __init__(self):
         super(ResTCN, self).__init__()
 
-        self.num_classes = 1
+        self.spatial_feat_dim = 32
+        self.num_classes = 4
         self.nhid = 128
         self.levels = 8
         self.kernel_size = 7
         self.dropout = .1
         self.channel_sizes = [self.nhid] * self.levels
 
-        # self.resnet = resnet18(pretrained=True)
-        # self.resnet.fc = nn.Sequential(nn.Linear(self.resnet.fc.in_features, self.num_features))
-
-        resnet = resnet18(pretrained=True)
-        self.num_features = resnet.fc.in_features
-        resnet.fc = Identity()
-        self.resnet = resnet
-
         self.tcn = TemporalConvNet(
-            self.num_features,
+            self.spatial_feat_dim,
             self.channel_sizes,
             kernel_size=self.kernel_size,
             dropout=self.dropout)
         self.linear = nn.Linear(self.channel_sizes[-1], self.num_classes)
 
+        self.model_conv = torchvision.models.resnet18(pretrained=True)
+        # for param in self.model_conv.parameters():
+        #     param.requires_grad = False
+
+        num_ftrs = self.model_conv.fc.in_features
+        # self.model_conv.fc = nn.Linear(num_ftrs, 4)
+        self.model_conv.fc = nn.Linear(num_ftrs, self.spatial_feat_dim)
+        # self.model_conv.fc = Identity()
+
+        # self.rnn = nn.LSTM(self.spatial_feat_dim, 64, 1, batch_first=True)
+        # self.linear = nn.Linear(64, 4)
+
     def forward(self, data):
-        z = torch.zeros([data.shape[0], data.shape[1], self.num_features]).cuda()
+        # t = 0
+        # x = data[:, t, :, :, :]
+        # output = self.model_conv(x)
+
+        z = torch.zeros([data.shape[0], data.shape[1], self.spatial_feat_dim]).cuda()
         for t in range(data.size(1)):
-            x = self.resnet(data[:, t, :, :, :])
+            x = self.model_conv(data[:, t, :, :, :])
             z[:, t, :] = x
+
+        # y, _ = self.rnn(z)
+        # output = self.linear(torch.sum(y, dim=1))
 
         z = z.transpose(1, 2)
         y = self.tcn(z)
-        output = self.linear(y[:, :, -1])
+        # output = self.linear(y[:, :, -1])
+        output = self.linear(torch.sum(y, dim=2))
+
         return output
