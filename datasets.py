a9e3d674432a0774b7a38486769e330fee317307
abedicodes <73629756+abedicodes@users.noreply.github.com>
2021-06-14 21:24:46 -0400

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -5,20 +5,19 @@ import os
 
 class VideoDataset(Dataset):
 
-    def __init__(self, csv_file, root, transform=None):
-        self.dataframe = pd.read_csv(csv_file)
-        self.transform = transform
+    def __init__(self, csv, root, transform=None):
 
+        self.dataframe = pd.read_csv(csv)
+        self.transform = transform
         self.root = root
 
     def __len__(self):
         return len(self.dataframe)
 
     def __getitem__(self, index):
-        label = self.dataframe.iloc[index].label
 
-        video = os.path.join(self.root,
-                             self.dataframe.iloc[index].path)
+        label = self.dataframe.iloc[index].label
+        video = os.path.join(self.root, self.dataframe.iloc[index].path)
         video = self.transform(video)
 
         return video, label
